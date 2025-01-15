30687e732e0d70f9d00e51df902aa1de8995d84a
TheoThenault <theo.thenault@gmail.com>
2025-01-15 14:44:51 +0100

    mdr

---
+++ Scripts/layer.gd
@@ -0,0 +1,29 @@
+extends TextureButton
+
+@export
+var numero : int
+
+@export
+var bpm : int
+
+@export
+var instrument : String
+
+@export
+var note : int
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("Nouvelle layer créée")
+
+func setup(num_ : int, bpm_ : int, instru_ : String, note_ : int) -> void:
+	numero = num_
+	bpm = bpm_
+	instrument = instru_
+	note = note_
+	pass
+
+func _on_pressed() -> void:
+	print_rich("[wave]Layer pressed[/wave]")
+	GAME.current_layer = numero
+	
